41bc903083e23601ccd0ad6da80f5cde715e3728
Nurlan13b <nurlanuraliyev@gmail.com>
2023-09-26 14:33:09 -0700

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,40 +1,23 @@
+      
+    //    setTimeout(function() {
+    //     document.getElementById("loading-text").style.display = "none";
+    // }, 5000); 
 
 
-// var userConfirmed = confirm("Do you want to change the background color to black?");
-// if (userConfirmed) {
-//   document.body.style.backgroundColor = "black";
-//   var userAge = prompt("Please enter your age:")
-//   if (userAge !== null &&!isNaN(userAge))
-//   if (userAge > 18) {
-//     //alert("You are over 18 years old. You may proceed.");
-//   } else {
-//     alert("You are under 18 years old. Access denied.");
-//   }
 
-// }
-// else {
-//     document.body.style.backgroundColor = "red";}
-    
-class User {
-  constructor(ad, soyad, password) {
-    this.ad = ad;
-    this.soyad = soyad;
-    this.password = password;
-  }
+    const inputElement = document.getElementById("customInput");
 
-  fullname() {
-    return `${this.ad} ${this.soyad}`;
-  }
-
-  htmlFullname() {
-    const fullname = this.fullname();
-    const html_fullname = `<p>${fullname.toUpperCase()}</p>`;
-    return html_fullname;
-  }
-}
-
-const istifadeci = new User("Yashar","Guseynov","password");
-const outputDiv = document.getElementById("output");
-outputDiv.innerHTML=istifadeci.htmlFullname();
-
-
+    inputElement.addEventListener("keydown", function(event) {
+        if (event.key === " ") {
+            
+            event.preventDefault();
+            this.value = this.value.replace(/\s/g, "");
+        } else if (event.key === "Backspace") {
+            
+            const cursorPosition = this.selectionStart;
+            this.value = this.value.slice(0, cursorPosition) + " " + this.value.slice(cursorPosition);
+            this.selectionStart = cursorPosition + 1;
+            this.selectionEnd = cursorPosition + 1;
+            event.preventDefault();
+        }
+    });
